b5ebb1d671ad9b22f80c2327b4824a9dc2e32506
백서현 <127273060+seohyeon0501@users.noreply.github.com>
2023-04-11 13:23:13 +0900

    Add files via upload

---
+++ 0411cal문제분석.py
@@ -0,0 +1,22 @@
+'''
+2023-04-11
+백서현
+if,elif,else
+
+
+'''
+
+op=int(input("연산자 입력:"))
+num1=int(input)("첫번쨰 숫자 입력")
+num2=int(input)("두번쨰 숫자 입력")
+
+if op=="+":
+    print(num1+num2)
+elif op=="-":
+    print(num1-num2)
+elif op=="*":
+    print(num1*num2)
+else:
+    print(num1/num2)
+    
+  
