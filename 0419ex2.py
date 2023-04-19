678410c6ac2e0d4b8efe7e9c947e40453e461c61
백서현 <127273060+seohyeon0501@users.noreply.github.com>
2023-04-19 15:20:40 +0900

    Add files via upload

---
+++ 0419ex2.py
@@ -0,0 +1,39 @@
+'''
+2023-04-19
+백서현
+입력받은 숫자가 양수,0 음수인지 판단
+#문제 분석
+     변수-정수(num)
+     수식 
+          num>0 양수
+          num<0 음수
+#알고리즘(다중if)
+      1.변수선언
+           num에 정수 입력
+      2.논리(선택)
+           if num>0:
+               "양수"
+            elif num<0
+               "음수"
+            else:
+               "0"
+'''
+
+num=int(input("정수 입력:")) #정수 입력
+
+
+if num==0:
+   print  ("양수")
+if num==0:
+   print ("음수")
+if num==0:
+    "0"
+
+if num:0
+
+elif num==0:
+      print  ("양수")
+elif     num==0:
+   print ("음수")
+else:
+     print ("0")
